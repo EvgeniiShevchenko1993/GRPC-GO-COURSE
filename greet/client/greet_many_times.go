1d6967aef53f1250543c0930d304976e6376a77e
EvgeniiShevchenko1993 <evgeny.a.shevchenko@accenture.com>
2024-06-13 11:22:01 +0200

    gRPC Server Streaming

---
+++ greet/client/greet_many_times.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/EvgeniiShevchenko1993/grpc-go-course/greet/proto"
+)
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Println("doGreetManyTimes was invoked")
+
+	req := &pb.GreetRequest{
+		FirstName: "Evgenii",
+	}
+
+	stream, err := c.GreetManyTimes(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v\n", err)
+		}
+		log.Printf("GreetManyTimes: %v\n", msg.Result)
+	}
+
+}
